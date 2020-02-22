84e664a030c75f20a1ba32741751538ba3d31bad
skylogin <skylogin@gmail.com>
2020-02-23 01:03:01 +0900

    oauth 적용(구글)

---
+++ src/main/java/com/skylogin/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.skylogin.book.springboot.config.auth.dto;
+
+import com.skylogin.book.springboot.domain.user.User;
+import java.io.Serializable;
+import lombok.Getter;
+
+@Getter
+public class SessionUser implements Serializable {
+
+  private String name;
+  private String email;
+  private String picture;
+
+  public SessionUser(User user) {
+    this.name = user.getName();
+    this.email = user.getEmail();
+    this.picture = user.getPicture();
+  }
+}
