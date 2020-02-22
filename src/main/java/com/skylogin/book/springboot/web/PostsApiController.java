757aef83b478eaf0c2a75aec01df6fa13c86aae7
skylogin <skylogin@gmail.com>
2020-02-22 23:09:25 +0900

    수정/삭제기능 추가

--- src/main/java/com/skylogin/book/springboot/web/PostsApiController.java
+++ src/main/java/com/skylogin/book/springboot/web/PostsApiController.java
@@ -5,6 +5,7 @@ import com.skylogin.book.springboot.web.dto.PostsResponseDto;
 import com.skylogin.book.springboot.web.dto.PostsSaveRequestDto;
 import com.skylogin.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,5 +32,11 @@ public class PostsApiController {
   @GetMapping("/api/v1/posts/{id}")
   public PostsResponseDto findById(@PathVariable Long id) {
     return postsService.findById(id);
+  }
+
+  @DeleteMapping("/api/v1/posts/{id}")
+  public Long delete(@PathVariable Long id) {
+    postsService.delete(id);
+    return id;
   }
 }
