16dd3b7eb50c81aa5d701d50f856b93a4c13e8e0
skylogin <skylogin@gmail.com>
2020-02-22 22:53:20 +0900

    index페이지에 게시글 가져오는 기능 추가

--- src/main/java/com/skylogin/book/springboot/servic/posts/PostsService.java
+++ src/main/java/com/skylogin/book/springboot/servic/posts/PostsService.java
@@ -2,9 +2,12 @@ package com.skylogin.book.springboot.servic.posts;
 
 import com.skylogin.book.springboot.domain.posts.Posts;
 import com.skylogin.book.springboot.domain.posts.PostsRepository;
+import com.skylogin.book.springboot.web.dto.PostsListResponseDto;
 import com.skylogin.book.springboot.web.dto.PostsResponseDto;
 import com.skylogin.book.springboot.web.dto.PostsSaveRequestDto;
 import com.skylogin.book.springboot.web.dto.PostsUpdateRequestDto;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,5 +39,12 @@ public class PostsService {
         .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
     return new PostsResponseDto(entity);
+  }
+
+  @Transactional(readOnly = true)
+  public List<PostsListResponseDto> findAllDesc() {
+    return postsRepository.findAllDesc().stream()
+        .map(PostsListResponseDto::new)
+        .collect(Collectors.toList());
   }
 }
--- src/main/java/com/skylogin/book/springboot/web/IndexController.java
+++ src/main/java/com/skylogin/book/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.skylogin.book.springboot.web;
 
+import com.skylogin.book.springboot.servic.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-  @GetMapping("/index")
-  public String index() {
+  private final PostsService postsService;
+
+  @GetMapping("/")
+  public String index(Model model) {
+    model.addAttribute("posts", postsService.findAllDesc());
     return "index";
   }
 
