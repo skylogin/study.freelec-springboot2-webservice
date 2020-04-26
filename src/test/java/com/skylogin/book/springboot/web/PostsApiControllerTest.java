c800cc13aab22e3a2658e0321fa4565a725a9600
skylogin <skylogin@gmail.com>
2020-04-26 22:06:52 +0900

    네이버 인증 추가

--- src/test/java/com/skylogin/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/skylogin/book/springboot/web/PostsApiControllerTest.java
@@ -1,7 +1,12 @@
 package com.skylogin.book.springboot.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.skylogin.book.springboot.domain.posts.Posts;
 import com.skylogin.book.springboot.domain.posts.PostsRepository;
 import com.skylogin.book.springboot.web.dto.PostsSaveRequestDto;
@@ -9,6 +14,7 @@ import com.skylogin.book.springboot.web.dto.PostsUpdateRequestDto;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +23,12 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -34,13 +42,25 @@ public class PostsApiControllerTest {
 
   @Autowired
   private PostsRepository postsRepository;
+  @Autowired
+  private WebApplicationContext context;
+  private MockMvc mvc;
 
   @After
   public void tearDown() throws Exception {
     postsRepository.deleteAll();
   }
 
+  @Before
+  public void setup() {
+    mvc = MockMvcBuilders
+        .webAppContextSetup(context)
+        .apply(springSecurity())
+        .build();
+  }
+
   @Test
+  @WithMockUser(roles = "USER")
   public void Posts_등록된다() throws Exception {
     //given
     String title = "title";
@@ -54,11 +74,15 @@ public class PostsApiControllerTest {
     String url = "http://localhost:" + port + "/api/v1/posts";
 
     //when
-    ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+//    ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+    mvc.perform(post(url)
+        .contentType(MediaType.APPLICATION_JSON_UTF8)
+        .content(new ObjectMapper().writeValueAsString(requestDto)))
+        .andExpect(status().isOk());
 
     //then
-    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(responseEntity.getBody()).isGreaterThan(0L);
+//    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+//    assertThat(responseEntity.getBody()).isGreaterThan(0L);
 
     List<Posts> all = postsRepository.findAll();
     assertThat(all.get(0).getTitle()).isEqualTo(title);
@@ -66,6 +90,7 @@ public class PostsApiControllerTest {
   }
 
   @Test
+  @WithMockUser(roles = "USER")
   public void Posts_수정된다() throws Exception {
     //given
     Posts savedPosts = postsRepository.save(
@@ -90,12 +115,16 @@ public class PostsApiControllerTest {
     HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
 
     //when
-    ResponseEntity<Long> responseEntity = restTemplate
-        .exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+//    ResponseEntity<Long> responseEntity = restTemplate
+//        .exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+    mvc.perform(put(url)
+        .contentType(MediaType.APPLICATION_JSON_UTF8)
+        .content(new ObjectMapper().writeValueAsString(requestDto)))
+        .andExpect(status().isOk());
 
     //then
-    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(responseEntity.getBody()).isGreaterThan(0L);
+//    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+//    assertThat(responseEntity.getBody()).isGreaterThan(0L);
 
     List<Posts> all = postsRepository.findAll();
     assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
