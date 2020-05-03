173d016ce0e85cc816a174de136be526c2006802
skylogin <skylogin@gmail.com>
2020-05-04 00:45:38 +0900

    배포쉘 추가 및 아티팩트 이름 뒤에 년월일시분초 추가

---
+++ scripts/start.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(disname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+REPOSITORY=/home/ec2-user/app/step3
+PROJECT_NAME=study.freelec-springboot2-webservice
+
+echo "> Build 파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+IDLE_PROFILE=$(find_idle_profile)
+nohup java -jar \
+        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-$IDLE_PROFILE.properties \
+        -Dspring.profiles.active=$IDLE_PROFILE \
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
